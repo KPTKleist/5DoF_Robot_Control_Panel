e0ce57c0ed3feb4a1cf3e7f198cd8c251f9dd64b
KPTKleist <KPTKleist@users.noreply.github.com>
2026-05-16 16:29:33 -0400

    Update 5DoF_Direct_Control.ino
    Updated PWM direct control example

--- 5DoF_Direct_Control/5DoF_Direct_Control.ino
+++ 5DoF_Direct_Control/5DoF_Direct_Control.ino
@@ -14,12 +14,32 @@ void setup() {
   
   pca9685.setPWMFreq(50);
 
+  /*
   pca9685.setPWM(0, 0, 220); //315
   pca9685.setPWM(1, 0, 360); //310
   pca9685.setPWM(2, 0, 215); //310
   pca9685.setPWM(3, 0, 480); //312
   pca9685.setPWM(4, 0, 307); //307
   pca9685.setPWM(5, 0, 280); //280~365
+  */
+  
+  //Set joint angles here
+  int16_t jointAngle0 = -95;
+  int16_t jointAngle1 = 50;
+  int16_t jointAngle2 = -95;
+  int16_t jointAngle3 = 168;
+  int16_t jointAngle4 = 0;
+  int16_t jointAngle5 = 0; //0 is closed, 85 is open
+
+  int16_t PWM_Command[6] = {315+jointAngle0, 310+jointAngle1, 310+jointAngle2, 312+jointAngle3, 307+jointAngle4, 280+jointAngle5};
+
+  pca9685.setPWM(0, 0, PWM_Command[0]); //315
+  pca9685.setPWM(1, 0, PWM_Command[1]); //310
+  pca9685.setPWM(2, 0, PWM_Command[2]); //310
+  pca9685.setPWM(3, 0, PWM_Command[3]); //312
+  pca9685.setPWM(4, 0, PWM_Command[4]); //307
+  pca9685.setPWM(5, 0, PWM_Command[5]); //280~365
+  
 
   delay(10);
 
